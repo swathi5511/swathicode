8a86e44e14f33bc7ee669ad8f4f8d354a3df8028
swathi5511 <dswathi5511@gmail.com>
2019-06-17 12:27:08 +0530

    Sample class cteated

---
+++ src/org/cts/git/Sample.java
@@ -0,0 +1,25 @@
+package org.cts.git;
+
+public class Sample {
+	public static class CorseDetails {
+
+		public void javaCorse() {
+
+			System.out.println(" java corse is started ");
+		}
+
+		public void oracleCorse() {
+
+			System.out.println(" oracle corse is not started ");
+		}
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+			CorseDetails cd = new CorseDetails();
+			cd.javaCorse();
+			cd.oracleCorse();
+		}
+
+	}
+}
